216041a0a42ea6b6b02d27aa8154fffbe05a5765
Arseniy <byykovaa1@student.bmstu.ru>
2020-12-02 01:30:50 +0300

    tests for pt2 done

--- lab6pt2module.rb
+++ lab6pt2module.rb
@@ -1,33 +1,33 @@
 module EnumCalc
-  
   def self.newCalc(eps)
     overallSum = 0.0
     x = 0.0
     n = 100000
-    exact = 1/3.to_f
-
-  
-   (Enumerator.new do |y|
-      precision = 1.0/n
-      (0..(n-1)).each do |_i|
-          overallSum += innerFunction(x)*precision
-          x+= precision  
-          y << overallSum  
-      end
+    exact = 1 / 3.to_f
+    inpstring = eps
+    begin
+      Integer(inpstring)
+    rescue
+      #puts "Your input is not an integer"
+      return "Your input is not an integer"
     end
-    
-  ).take_while { |y| ((y-exact).abs > eps)}
+    (Enumerator.new do |y|
+      precision = 1.0 / n
+      (0..(n - 1)).each do |_i|
+        overallSum += innerFunction(x) * precision
+        x += precision
+        y << overallSum
+      end
+    end).take_while { |y| ((y - exact).abs > eps) }
 
- overallSum
+    return overallSum
   end
 
-    def self.innerFunction(x)
-      innerFuncRes = 0.0
-      innerFuncRes=x*Math.sqrt((1-(x*x)).abs)
-      print()
-     
-      return innerFuncRes
-    end
+  def self.innerFunction(x)
+    innerFuncRes = 0.0
+    innerFuncRes = x * Math.sqrt((1 - (x * x)).abs)
+    print()
+
+    return innerFuncRes
+  end
 end
-
-
