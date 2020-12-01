dc7a0607af0d5790278a8f50a9794628224bc096
Arseniy <byykovaa1@student.bmstu.ru>
2020-12-01 22:46:41 +0300

    tests for pt1 done

--- lab6pt1console.rb
+++ lab6pt1console.rb
@@ -1,5 +1,5 @@
-require_relative 'lab6pt1module.rb'
+require_relative "lab6pt1module.rb"
 puts("enter precision of your wish")
 inputString = gets
 
-puts(Integral.input(Integer(inputString)))
+puts(Integral.input(inputString))
--- lab6pt1module.rb
+++ lab6pt1module.rb
@@ -1,38 +1,34 @@
 module Integral
-def Integral.input(numberOfIterations)
-    
-    
+  def Integral.input(numberOfIterations)
+    inpstring = String.new
     upper = 1.0
     lower = 0.0
-    counter = 0.0 
+    counter = 0.0
     overallSum = 0.0
+    inpstring = numberOfIterations
+    begin
+      Integer(inpstring)
+    rescue
+      #puts "Your input is not an integer"
+      return "Your input is not an integer"
+    end
+    precision = numberOfIterations.to_i
 
-    precision = numberOfIterations
-    
-   if precision.
-    iteration = (upper-lower)/precision
-    overallSum = (innerFunction(1)-innerFunction(0))/2
-   for i in 1..(precision-1) do
-    counter += iteration
-       overallSum += innerFunction(counter)
-   
-   end
-   overallSum = overallSum*iteration
-return overallSum
-end
+    iteration = (upper - lower) / precision
+    overallSum = (innerFunction(1) - innerFunction(0)) / 2
+    for i in 1..(precision - 1)
+      counter += iteration
+      overallSum += innerFunction(counter)
+    end
+    overallSum = overallSum * iteration
+    return overallSum
+  end
 
-def Integral.innerFunction(x)
+  def Integral.innerFunction(x)
     innerFuncRes = 0.0
-    innerFuncRes=x*Math.sqrt((1-(x*x)).abs)
+    innerFuncRes = x * Math.sqrt((1 - (x * x)).abs)
     print()
-   
+
     return innerFuncRes
+  end
 end
-
-   
-
-end
-
-
-
-
