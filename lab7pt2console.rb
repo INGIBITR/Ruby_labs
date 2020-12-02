8f70695c7fcae1c3ea5cc76effa36d4db0e73567
Arseniy <byykovaa1@student.bmstu.ru>
2020-12-02 17:02:46 +0300

    tests for pt2 done

--- lab7pt2console.rb
+++ lab7pt2console.rb
@@ -1,8 +1,19 @@
 require_relative "lab7pt2module.rb"
 include Strings
 rect = Rectangle.new(5, 8)
-puts rect.calc_area
+puts("enter length: \n")
+ll = gets.chomp
+puts("enter width: \n")
+ww = gets.chomp
+rect = Rectangle.new(ll.to_f, ww.to_f)
+puts ("Area = #{rect.calc_area}")
 puts rect.print_properties
-parall = Parallelepiped.new(3,4,5)
-puts parall.calc_volume
+puts("enter length: \n")
+ll = gets.chomp
+puts("enter width: \n")
+ww = gets.chomp
+puts("enter height: \n")
+hh = gets.chomp
+parall = Parallelepiped.new(ll.to_f, ww.to_f, hh.to_f)
+puts ("Volume = #{parall.calc_volume}")
 puts parall.print_properties
--- lab7pt2module.rb
+++ lab7pt2module.rb
@@ -1,44 +1,33 @@
-module Strings 
+module Strings
+  class Rectangle
+    attr_accessor :name, :age
 
-    class Rectangle
-        attr_accessor :name, :age
-        def initialize(length, width)
-            @length = length.to_f
-            @width = width.to_f
-       
-        end 
-
-        def calc_area
-            return @length * @width
-        end
-
-        def print_properties
-            return "length: #{@length}\nwidth: #{@width}"
-        end
-
+    def initialize(length, width)
+      @length = length
+      @width = width
     end
 
-    class Parallelepiped < Rectangle
-        def initialize(length, width, height)
-            super(length, width)
-            @height = height.to_f
-        end
-
-        def calc_volume
-
-           return calc_area * @height
-        end
-
-        def print_properties
-            return "#{super}\nheight: #{@height}"
-        end
+    def calc_area
+      return @length * @width
     end
-    
-    
-    
-    
 
+    def print_properties
+      return "length: #{@length}\nwidth: #{@width}"
+    end
+  end
 
+  class Parallelepiped < Rectangle
+    def initialize(length, width, height)
+      super(length, width)
+      @height = height
+    end
 
+    def calc_volume
+      return calc_area * @height
+    end
+
+    def print_properties
+      return "#{super}\nheight: #{@height}"
+    end
+  end
 end
-
