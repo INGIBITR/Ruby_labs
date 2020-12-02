76204ed7fa242af52663dddcc41f76f3e50e1795
Arseniy <byykovaa1@student.bmstu.ru>
2020-12-02 16:22:39 +0300

    tests for pt1 done

--- lab7pt1console.rb
+++ lab7pt1console.rb
@@ -1,2 +1,2 @@
 require_relative "lab7pt1module.rb"
-Lab7pt1.output
+Lab7pt1.output("lab7text.txt")
--- lab7pt1module.rb
+++ lab7pt1module.rb
@@ -1,6 +1,12 @@
 module Lab7pt1
-  def self.output
-    config = IO.read("lab7text.txt")
+  def self.output(filename)
+    begin
+      config = IO.read(filename)
+    rescue
+      puts "Error reading file"
+      return "Error reading file"
+    end
+
     arrayOfData = config.split("\n")
 
     for checker in 0..arrayOfData.count - 1
@@ -8,11 +14,18 @@ module Lab7pt1
       puts("\n")
 
       if (tempstring[0] == "1")
-        puts "It is a segment"
         tempstring = tempstring[2..tempstring.length]
         #puts "modified string #{tempstring}"
         coords = tempstring.split
-
+        begin
+          for i in 0..3
+            Integer(coords[i])
+          end
+        rescue
+          puts "You have a mistake in your input"
+          return "You have a mistake in your input"
+        end
+        puts "It is a segment"
         puts "Its coords are: 
        x1:#{coords[0]}
        y1:#{coords[1]}
@@ -20,11 +33,20 @@ module Lab7pt1
        y2:#{coords[3]}
        and its length is 
        #{Math.sqrt(((coords[2].to_f - coords[0].to_f) ** 2) +
-                   ((coords[3].to_f - coords[1].to_f) ** 2))}"
+                   ((coords[3].to_f - coords[1].to_f) ** 2))}
+                   #{coords[4]} "
       elsif (tempstring[0] == "2")
-        puts "It is a circle"
         tempstring = tempstring[2..tempstring.length]
         coords = tempstring.split
+        begin
+          for i in 0..2
+            Integer(coords[i])
+          end
+        rescue
+          puts "You have a mistake in your input"
+          return "You have a mistake in your input"
+        end
+        puts "It is a circle"
         puts "Its coords are:
         x: #{coords[0]}
         y: #{coords[1]}
