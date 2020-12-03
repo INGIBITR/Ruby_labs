d5688565c1c84743a4192cb9fe009c013d0e9893
Arseniy <byykovaa1@student.bmstu.ru>
2020-12-03 15:27:06 +0300

    lab 9 init

---
+++ Lab9_app/app/controllers/palindrome_controller.rb
@@ -0,0 +1,38 @@
+class PalindromeController < ApplicationController
+  $currentnumber = []
+  $currentiteration = []
+  def input
+  end
+
+  def view
+    num = params[:num].to_i
+    @result = palindrome?(num)
+
+  end
+
+
+  
+    def palindrome?(x)
+        iteration = 0
+        $currentnumber[0] = x
+        $currentiteration[0] = 0
+
+      while x.to_s != x.to_s.reverse 
+        iteration +=1
+        if  x.to_s != x.to_s.reverse 
+            
+            $currentiteration[iteration] = iteration
+            rev = (x.to_s.reverse).to_i
+            x = rev + x
+            $currentnumber[iteration] = x
+            print("x after iteration #{iteration}:\n")
+            print(x,"\n")
+           # print("current iteration: #{iteration}")
+              
+        elsif x.to_s == x.to_s.reverse 
+            return x
+        end
+      end
+      return x
+    end
+end
