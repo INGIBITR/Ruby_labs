dadaec73e6de9e2a97e5ad2fa471fb5e51b9c75c
Arseniy <byykovaa1@student.bmstu.ru>
2020-12-04 01:21:10 +0300

    done tests

--- Lab9_app/app/controllers/lab_controller.rb
+++ Lab9_app/app/controllers/lab_controller.rb
@@ -7,29 +7,22 @@ class LabController < ApplicationController
   def calculate
     x_param = params[:x]
     result = ''
-    @currentnumber = []
-    @currentiteration = []
     iteration = 0
-    @currentnumber[0] = x_param
-    @currentiteration[0] = 0
     values = []
-    
+
     if x_param == ''
       result = 'Заполните поле'
     elsif x_param.to_i <= 0
       result = 'Введите число больше нуля'
     elsif x_param.to_i >= 0
-      values.push(x_param)
-      
+      values.push(x_param.to_i)
 
       while x_param.to_s != x_param.to_s.reverse
         iteration += 1
         if x_param.to_s != x_param.to_s.reverse
-          @currentiteration[iteration] = iteration
+
           rev = x_param.to_s.reverse.to_i
           x_param = rev + x_param.to_i
-          @currentnumber[iteration] = x_param
-          print("x after iteration #{iteration}:\n")
           # print(x, "\n")
 
         end
@@ -38,7 +31,6 @@ class LabController < ApplicationController
       result = values
     end
     # values.push(30)
-    
 
     render json: { result: result }
   end
--- Lab9_app/test/controllers/lab8_controller_test.rb
+++ Lab9_app/test/controllers/lab8_controller_test.rb
@@ -1,26 +1,20 @@
 # frozen_string_literal: true
 
-require "./test/test_helper"
+require './test/test_helper'
 
 class Lab8ControllerTest < ActionController::TestCase
   def setup
-    @controller = PalindromeController.new
+    @controller = LabController.new
   end
 
-  test "should get input" do
-    get :input
+  test 'should get index' do
+    get :index
     assert_response :success
   end
 
-  test "should get view" do
-    get :view
+  test 'should calculate' do
+    get :calculate, params: { 'x' => '29472' }, format: :json
     assert_response :success
-  end
-
-  test "should calculate" do
-    get :view, params: { "num" => "5698" }
-    assert_response :success
-
-    assert_select "#1", "91619"
+    assert_equal '{"result":[29472,56964,103929,1033230,1356531]}', @response.body
   end
 end
