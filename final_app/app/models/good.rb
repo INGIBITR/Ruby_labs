b8b65e7e0f6f9b78889dfe6d1dc9da20371a4186
Arseniy <byykovaa1@student.bmstu.ru>
2020-12-11 22:58:23 +0300

    clened up and corrected lab12

--- final_app/app/models/good.rb
+++
@@ -1,5 +0,0 @@
-class Good < ApplicationRecord
-  validates :title, presence: true
-  validates :price, presence: true
-  validates :amount, presence: true
-end
--- final_app/app/views/goods/_good.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! good, :id, :title, :price, :amount, :integer, :created_at, :updated_at
-json.url good_url(good, format: :json)
