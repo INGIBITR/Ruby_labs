0985a87008891f40bafc63c0f19f68b7fcd614f6
Arseniy <byykovaa1@student.bmstu.ru>
2020-12-11 22:58:23 +0300

    cleaned up and corrected lab12

--- lab12/db/migrate/20201129200943_create_cached_results.rb
+++ lab12/db/migrate/20201129200943_create_cached_results.rb
@@ -7,14 +7,5 @@ class CreateCachedResults < ActiveRecord::Migration[6.0]
       t.timestamps
     end
     add_index :cached_results, :n, unique: true
-    create_table :action_text_rich_texts do |t|
-      t.string :name, null: false
-      t.text :body, size: :long
-      t.references :record, null: false, polymorphic: true, index: false
-
-      t.timestamps
-
-      t.index [:record_type, :record_id, :name], name: "index_action_text_rich_texts_uniqueness", unique: true
-    end
   end
 end
