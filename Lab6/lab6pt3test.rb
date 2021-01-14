b34e0390ce5b0dea3c7b2e7fd982743b310f84fb
INGIBITR <64781992+INGIBITR@users.noreply.github.com>
2021-01-14 15:23:57 +0300

    Add files via upload

--- Lab6/lab6pt3test.rb
+++ Lab6/lab6pt3test.rb
@@ -1,5 +1,7 @@
-require_relative 'lab6pt3module'
-require 'minitest/autorun'
+# frozen_string_literal: true
+
+require_relative "lab6pt3module"
+require "minitest/autorun"
 
 # Test Class
 class TestClass < Minitest::Test
@@ -8,26 +10,26 @@ class TestClass < Minitest::Test
   def setup; end
 
   def test_correct_int1
-    assert_in_delta(7.58467, Integral.input(1000, 1), 0.01)
+    assert_in_delta(7.58467, Integral.blockintprg(1000, 1), 0.01)
   end
 
   def test_correct1_int1
-    assert_in_delta(7.58467, Integral.input(10_000, 1), 0.001)
+    assert_in_delta(7.58467, Integral.blockintprg(10_000, 1), 0.001)
   end
 
   def test_correct_int2
-    assert_in_delta(-0.3768714, Integral.input(1000, 2), 0.01)
+    assert_in_delta(-0.3768714, Integral.blockintprg(1000, 2), 0.01)
   end
 
   def test_correct1_int2
-    assert_in_delta(-0.3768714, Integral.input(10_000, 2), 0.001)
+    assert_in_delta(-0.3768714, Integral.blockintprg(10_000, 2), 0.001)
   end
 
   def test_input_failure
-    assert_equal(Integral.input('string', 5), 'Your input is not an integer')
+    assert_equal(Integral.blockintprg("string", 5), "Your input is not an integer")
   end
 
-  def test_input_failure
-    assert_equal(Integral.input(5000, 'string'), 'Your input is not an integer')
+  def test_input_failure_2
+    assert_equal(Integral.blockintprg(5000, "string"), "Your input is not an integer")
   end
 end
